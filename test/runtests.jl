149df24f1286f5cc881aaf9bef877ce0390b3dbd
Yue Ren <yue.ren.kl@gmail.com>
2025-05-25 11:39:37 +0100

    fix: test
    switch from checking cardinality of point set to sum of multiplicities

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,16 @@ using Test
 
 @testset "TropicalHomotopies.jl" begin
     import Oscar.matrix, Oscar.echelon_form, Oscar.QQ, Oscar.polynomial_ring, Oscar.randseed!, Oscar.set_seed!
-    
+
     function straight_line_rigidity_example_test()
         # define matrix encoding linear ideal
         linearMatrix = matrix(QQ, [1 1 0 0 0 0 0 0 0 0; -1 0 -1 -1 0 0 0 0 0 0; 0 -1 1 0 -1 0 0 0 0 0; 0 0 0 1 1 0 0 0 0 0; 0 0 0 0 0 1 1 0 0 0; 0 0 0 0 0 -1 0 -1 -1 0; 0 0 0 0 0 0 -1 1 0 -1; 0 0 0 0 0 0 0 0 1 1])
-        
+
         # reduce linearMatrix to row echelon form
         linearMatrix = echelon_form(linearMatrix, trim=true)
         R, (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10) = polynomial_ring(QQ, 10)
         M = matroid(linearMatrix)
-        
+
         # define hypersurface supports
         randseed!(31415296) # seed to reproduce bug
         targetSupports = Support[]
@@ -23,16 +23,16 @@ using Test
         end
         # add an extra hypersurface to cut the common lineality space
         push!(targetSupports, support([point([1,0,0,0,0,0,0,0,0,0]),point([0,0,0,0,0,0,0,0,0,0])],[1,3]))
-        
+
         # define the target support
         targetSupport = mixed_support(targetSupports)
         Δ, σ = starting_data(targetSupport, M)
         T = tracker(Δ, targetSupport, [σ], path=:straight_line)
 
-        
-        @test length(stable_intersection(T)) == 4
+        Sigma = stable_intersection(T)
+        @test sum(Sigma[2]) == 4
     end
 
     straight_line_rigidity_example_test()
-    
+
 end
